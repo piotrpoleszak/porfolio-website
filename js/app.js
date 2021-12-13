6942079cf6a540726de0631d701d9902dfb8ba52
Poleszak <poleszak@protonmail.com>
2021-12-13 02:29:52 +0100

    Qualification section - changing sections not working

--- js/app.js
+++ js/app.js
@@ -51,3 +51,30 @@ $('.navbar-nav>li>a').on('click', function()
 {
   $('.navbar-collapse').collapse('hide');
 });
+
+
+// QUALIFICATION TABS
+// QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>
+{
+    tab.addEventListener('click', () =>
+    {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => 
+            {
+                tabContent.classList.remove('qualification__active')
+            })
+            target.classList.add('qualification__active')
+
+        tabs.forEach(tab => 
+            {
+                tab.classList.remove('qualification__active')
+            })
+        tab.classList.add('qualification__active')
+    })
+})
+
