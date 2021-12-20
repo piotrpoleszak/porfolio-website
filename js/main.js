9c93c97a7214814726b57f06dc25cebd56a73d37
Poleszak <poleszak@protonmail.com>
2021-12-20 22:55:15 +0100

    Scroll-Top button

--- js/main.js
+++ js/main.js
@@ -76,3 +76,12 @@ tabs.forEach(tab =>
         tab.classList.add('qualification__active')
     })
 })
+
+
+//==================== SHOW SCROLL UP ====================\\
+function scrollUp()
+{
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
